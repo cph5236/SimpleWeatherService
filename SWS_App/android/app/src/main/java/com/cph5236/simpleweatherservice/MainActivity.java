003a2cf58bc3128f15f5e8ea00015768a3632782
cph5236 <hanlinconnor@gmail.com>
2026-03-12 14:30:18 -0400

    Add CI/CD workflows, SWS_App with Capacitor scaffolding

---
+++ SWS_App/android/app/src/main/java/com/cph5236/simpleweatherservice/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cph5236.simpleweatherservice;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- SWS_App/src/App.tsx
+++ SWS_App/src/App.tsx
@@ -3,7 +3,7 @@ import { HomePage } from './pages/HomePage'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/SimpleWeatherService">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<HomePage />} />
