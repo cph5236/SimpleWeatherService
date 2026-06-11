530672e211ede3ae27252e12063b3f3d77ed242e
cph5236 <33178603+cph5236@users.noreply.github.com>
2026-06-11 15:33:15 +0000

    Deploying to gh-pages from @ cph5236/SimpleWeatherService@4711455aba1775a44bb2b26444defd90c8ee577f 🚀

--- SWS_App/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'com.cph5236.simpleweatherservice',
-  appName: 'Simple Weather Service',
-  webDir: 'dist',
-}
-
-export default config
